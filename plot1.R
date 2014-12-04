5df9545cf23200c9cd4c4d07d4f471961bbba840
MF Coursera Data Science <fernamn@yahoo.com>
2014-12-03 22:20:29 -0500

    Added histogram

--- plot1.R
+++ plot1.R
@@ -1,6 +1,12 @@
-#Read in data file and subset dates 2007-02-01 and 2007-02-02
+#plot1.R
+#Generates histogram of Global Active Power between 2007-02-01 and 2007-02-02
+
+#Read in data file and subset data recorded between 2007-02-01 and 2007-02-02
 pdata <- subset  (read.csv ("./household_power_consumption.txt", sep=";", header=TRUE, na.strings=c("?"), stringsAsFactors=FALSE )   , Date=="1/2/2007" | Date=="2/2/2007")
 
-#Cast date and time columns into datetime and date types
-pdata$DateTime <- strptime (paste (pdata$Date, pdata$Time, sep=" "),format ="%d/%m/%Y %T" )
-pdata$Date <- as.Date(pdata$Date,format="%d/%m/%Y")
+#Generate historgram
+hist (pdata$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col="red")
+
+#Copy to PNG file
+dev.copy (png, file="plot1.png")
+dev.off()
